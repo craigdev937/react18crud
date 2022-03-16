369a1b6e03808845e09d982d376bd045c8a6b235
Craig <indigomx9@gmail.com>
2022-03-16 16:13:31 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# CRUD
+
+Initial setup of the Project.  Created with React 18, Webpack, Babel, useContext and useReducer.
+
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, perspiciatis. Ipsum esse inventore necessitatibus harum voluptatem, quae ducimus delectus veritatis commodi vel aspernatur, debitis modi. Pariatur amet illo ex? Consequuntur.</p>
+        </React.Fragment>
+    );
+};
+
+
+
