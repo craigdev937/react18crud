63f30d97ca1b36590df7ec7dbb8bebed824062ad
Craig <indigomx9@gmail.com>
2022-03-16 23:57:33 -0400

    Updated the TaskList component.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import { Main } from "../routes/Main";
+import { GlocalProvider } from "../global/GState";
 
 export const App = () => {
     return (
         <React.Fragment>
-            <Main />
+            <GlocalProvider>
+                <Main />
+            </GlocalProvider>
         </React.Fragment>
     );
 };
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { GlobalContext } from "../global/GState";
 
 export const TaskList = () => {
+    const { tasks, deleteTask, toggleTaskDone } = 
+    React.useContext(GlobalContext);
+
     return (
         <React.Fragment>
-            <h1>TaskList</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, perspiciatis. Ipsum esse inventore necessitatibus harum voluptatem, quae ducimus delectus veritatis commodi vel aspernatur, debitis modi. Pariatur amet illo ex? Consequuntur.</p>
+        {tasks.length > 0 ? (
+            <main>
+            {tasks.map((task) => (
+                <aside key={task.id}>
+                    <section className="text-left">
+                        <h1>{task.title}</h1>
+                        <h6>{task.id}</h6>
+                        <p>{task.description}</p>
+                        <button 
+                            onClick={() => toggleTaskDone(task.id)}
+                            >{task.done ? "Undone" : "Done"}
+                        </button>
+                        <div>
+                            <Link
+                                to={`/edit/${task.id}`}
+                                >Edit   
+                            </Link>
+                            <button
+                                onClick={() => deleteTask(task.id)}
+                                >Delete
+                            </button>
+                        </div>
+                    </section>
+                </aside>
+            ))}
+            </main>
+        ) : (
+            <p>No Tasks yet</p>
+        )}
         </React.Fragment>
     );
 };
